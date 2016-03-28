207352f23d296ba97d615897f5900f375e8937f7
vjfidalgo <vjfidalgo@gmail.com>
2016-03-28 13:41:10 +0100

    change on master 01

--- src/mySummation.f90
+++ src/mySummation.f90
@@ -7,9 +7,11 @@ implicit none
 integer :: sum, a
 
 print*, "This program performs summations. Enter 0 to stop."
+print*, "Now, this change is here to stay"
 print*, "and this too"
 print*, "the last one"
 print*, "one more ..."
+print*, "let's add a change here"
 open(unit=10, file="SumData.DAT")
 
 sum = 0

9fc11fc0e29c94eb9767c026d27c6c8ec79ef960
vjfidalgo <vjfidalgo@gmail.com>
2016-03-28 12:49:05 +0100

    Revert "Revert "first change""
    This reverts commit 4c5e5183a91cb77a31b21b712cf4b818ffa77016.

--- src/mySummation.f90
+++ src/mySummation.f90
@@ -7,6 +7,7 @@ implicit none
 integer :: sum, a
 
 print*, "This program performs summations. Enter 0 to stop."
+print*, "This program performs summations. Enter 0 to stop."
 open(unit=10, file="SumData.DAT")
 
 sum = 0
